75056abeb6f53fa170856665310edf168be1bd3b
Dante Calderon <mail@dantecalderon.dev>
2020-02-16 18:14:44 -0500

    Add shadow

--- components/Button.js
+++ components/Button.js
@@ -1,12 +1,32 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Button } from '@ui-kitten/components';
+import { BoxShadow } from 'react-native-shadow'
 
 const GameButton = ({ children, ...props}) => {
+  const shadowOpt = {
+    width: 400,
+    height:50,
+    color:"#000",
+    flex: 1,
+    border:2,
+    radius:10,
+    alignSelf: 'stretch',
+    textAlign: 'center',
+    opacity:0.2,
+    x: 0,
+    y: 0,
+    style:{
+      marginVertical: 0
+    }
+  }
+
   return (
-    <Button {...props} underlayColor={'blue'} textStyle={styles.text} style={styles.button}>
-      {children}
-    </Button>
+    <BoxShadow setting={shadowOpt}>
+      <Button {...props} underlayColor={'blue'} textStyle={styles.text} style={styles.button}>
+        {children}
+      </Button>
+    </BoxShadow>
   )
 }
 
@@ -14,12 +34,14 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: '#fd9712',
     minHeight: 44,
+    // width: 700,
+    alignSelf: 'stretch',
+    textAlign: 'center',
     borderTopWidth: 0,
     borderLeftWidth: 0,
     borderRightWidth: 0,
     borderColor: '#5c5c5c57',
     borderWidth: 4,
-    
     textShadowColor: '#7b7b7b40',
     textShadowOffset: {width:0, height: 0},
     textShadowRadius: 3,
