d26ed9282da2bbb3ada591eebc157fa5e1c56a97
Dante Calderon <mail@dantecalderon.dev>
2020-02-22 03:27:58 -0500

    Add game functionality (fix #6)

---
+++ components/Filler.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Text } from '@ui-kitten/components'
+
+const Filler = ({ wordToFill }) => {
+  return (
+    <View>
+      <Text style={styles.text}>{wordToFill}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#545454',
+    textTransform: 'uppercase',
+    fontSize: 20,
+    fontWeight: 'bold',
+    lineHeight: 28,
+    textAlign: 'center',
+    fontFamily: 'monospace, Courier'
+  }
+})
+
+export default Filler
--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,31 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, View } from 'react-native'
 import BackgroundContainer from '../components/BackgroundContainer'
 import Keyboard from '../components/Keyboard'
 import Man from '../components/Man'
 import { useWordManager } from '../hooks/useWordManager'
+import { Text } from '@ui-kitten/components'
+import Filler from '../components/Filler'
+import { Word } from '../interfaces/Word'
+import { fillWithLetter, areEqualsLowercase } from '../utils/word'
 
 const GameScreen = ({
   categoryId
 }) => {
-  const handlePressKey = (key) => {
-    nextWord()
-    console.log('Key  pressed: ', key)
-  }
 
-
+  const [wordToFill, setWordToFill] = useState<string>('')
+  
   const { word, nextWord, loading } = useWordManager({})
-  console.log("Dante: nextWord", nextWord)
-  console.log("Dante: loading", loading)
   console.log("Dante: word", word)
+  
+  const handlePressKey = (letter) => {
+    const existsLetter = word.name.split('').some(c => areEqualsLowercase(c, letter))
+    if(existsLetter) {
+      setWordToFill(wordToFill => fillWithLetter(word.name, wordToFill, letter))
+    } else {
+      // Restar vidas 
+    }
+  }
+  
+  useEffect(() => {
+    if(word && areEqualsLowercase(word.name, wordToFill)) {
+      console.log('Dante: GANASTEEEEEE')
+      nextWord()
+    }
+  }, [wordToFill])
 
+  useEffect(() => {
+    if(word) {
+      setWordToFill(word.name.replace(/[^\s]/gi, '-'))
+    }
+  },[word])
 
   return (
     <View style={styles.container}>
-      <BackgroundContainer>
-        <Man/>
-        <Keyboard onPressKey={handlePressKey}/>
-      </BackgroundContainer>
+      {
+        loading ?
+        <Text>Loading</Text>:
+        <BackgroundContainer>
+          <Man/>
+          <Filler wordToFill={wordToFill}/>
+          <Keyboard onPressKey={handlePressKey}/>
+        </BackgroundContainer>
+      }
     </View>
   )
 }
