e88ae337cded021659076415313b03a8ce7a3e03
Dante Calderon <mail@dantecalderon.dev>
2020-03-01 16:29:01 -0500

    Improve buttons and add link to github project and my website

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,8 @@
 import { useNavigation } from '@react-navigation/native'
 import * as React from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
+import { Platform, StyleSheet, Text, View, Linking } from 'react-native'
 import BackgroundContainer from '../components/BackgroundContainer'
-import Button from '../components/Button'
+import Button from '../components/GameButton'
 import LogoHome from '../components/LogoHome'
 
 export default function HomeScreen({ navigation }) {
@@ -15,11 +15,25 @@ export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <BackgroundContainer>
-        <Text>Score: 123</Text>
+        <Text></Text>
         <LogoHome />
-        <Button onPress={handlePressPlay} size='giant'>
-          PLAY NOW!
-        </Button>
+        <View>
+          <Button onPress={handlePressPlay} color='#fd9712' size='giant'>
+            PLAY NOW!
+          </Button>
+          <View style={styles.menuOptions}>
+            <Text onPress={() => Linking.openURL('https://dantecalderon.dev')} style={styles.info}>
+              by Dante Calderon
+            </Text>
+            <Text style={styles.info}>This Game is Open Source</Text>
+            <Text
+              onPress={() => Linking.openURL('https://github.com/dantehemerson/hangman-app')}
+              style={[styles.info, { color: 'skyblue' }]}
+            >
+              Check in Github
+            </Text>
+          </View>
+        </View>
       </BackgroundContainer>
     </View>
   )
@@ -32,5 +46,17 @@ HomeScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  menuOptions: {
+    marginBottom: 50,
+    paddingVertical: 20,
+    justifyContent: 'center',
+  },
+  info: {
+    textAlign: 'center',
+    fontSize: 10,
+    fontWeight: 'bold',
+    lineHeight: 15,
+    color: 'gray',
   },
 })
